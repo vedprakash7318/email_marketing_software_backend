80e4496f012351040724ae25636a3418c08ea483
DigiCoders Technologies <digicodersdevelopment@gmail.com>
2026-05-27 19:58:37 +0530

    solve error

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.get('/unsubscribe/:email', async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  app.get('*', (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
   });
 } else {
